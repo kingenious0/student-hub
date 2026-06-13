034de62e7ce8bef4cbd9a1961253c4f72fd1a77f
kingenious0-8358 <kingenious0@gmail.com>
2026-06-13 05:09:40 +0000

    chore: update Content-Security-Policy to allow Cloudflare challenges domain

--- next.config.ts
+++ next.config.ts
@@ -58,11 +58,11 @@ const nextConfig: NextConfig = {
           {
             key: 'Content-Security-Policy',
             value: "default-src 'self'; " +
-                   "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://*.clerk.accounts.dev https://clerk.lahustle.kingenious.xyz https://js.paystack.co https://checkout.paystack.com https://*.paystack.com https://www.googletagmanager.com https://*.googletagmanager.com; " +
-                   "connect-src 'self' https://*.clerk.accounts.dev wss://*.clerk.accounts.dev https://clerk.lahustle.kingenious.xyz wss://clerk.lahustle.kingenious.xyz https://api.paystack.co https://checkout.paystack.com https://*.paystack.com https://js.paystack.co https://api.cloudinary.com https://api.radar.io https://*.radar.io https://www.google-analytics.com https://*.google-analytics.com https://www.googletagmanager.com https://*.googletagmanager.com; " +
+                   "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://*.clerk.accounts.dev https://clerk.lahustle.kingenious.xyz https://js.paystack.co https://checkout.paystack.com https://*.paystack.com https://www.googletagmanager.com https://*.googletagmanager.com https://challenges.cloudflare.com; " +
+                   "connect-src 'self' https://*.clerk.accounts.dev wss://*.clerk.accounts.dev https://clerk.lahustle.kingenious.xyz wss://clerk.lahustle.kingenious.xyz https://api.paystack.co https://checkout.paystack.com https://*.paystack.com https://js.paystack.co https://api.cloudinary.com https://api.radar.io https://*.radar.io https://www.google-analytics.com https://*.google-analytics.com https://www.googletagmanager.com https://*.googletagmanager.com https://challenges.cloudflare.com; " +
                    "img-src 'self' data: blob: https:; " +
                    "style-src 'self' 'unsafe-inline' https://paystack.com https://*.paystack.com https://checkout.paystack.com; " +
-                   "frame-src 'self' https://js.paystack.co https://checkout.paystack.com https://*.paystack.com https://*.clerk.accounts.dev https://clerk.lahustle.kingenious.xyz; " +
+                   "frame-src 'self' https://js.paystack.co https://checkout.paystack.com https://*.paystack.com https://*.clerk.accounts.dev https://clerk.lahustle.kingenious.xyz https://challenges.cloudflare.com; " +
                    "worker-src 'self' blob:; " +
                    "media-src 'self' blob: data: https://res.cloudinary.com;",
           },
